4565071da576987ab2d2bfdf172822f0d98519b2
myagmarsurend <myagmarsuren@datacare.mn>
2024-03-21 13:00:23 +0800

    up

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,16 +2,16 @@ const jwt = require("jsonwebtoken");
 
 const jwtMiddleware = async (req, res, next) => {
   try {
-    const authorizationHeader = req.header("Authorization");
-    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
-      return res.status(401).json({ message: "Invalid token" });
-    }
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Authentication required" });
-    }
-    const decoded = jwt.verify(token, "trmo_app_key");
-    req.user = decoded;
+    // const authorizationHeader = req.header("Authorization");
+    // if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
+    //   return res.status(401).json({ message: "Invalid token" });
+    // }
+    // const token = req.headers.authorization?.split(" ")[1];
+    // if (!token) {
+    //   return res.status(401).json({ message: "Authentication required" });
+    // }
+    // const decoded = jwt.verify(token, "trmo_app_key");
+    // req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
